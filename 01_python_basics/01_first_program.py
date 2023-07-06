f359dfa0826cc3beeb53a3e7ae989309c8c6daf9
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-07-06 22:49:24 +0530

    commit

---
+++ 01_python_basics/01_first_program.py
@@ -0,0 +1,5 @@
+
+#hello world program in python
+
+print("\nHey Man! You are warmly welcomed to the world of Python")
+print("Hope to have a better tommorow")
---
+++ 01_python_basics/02_area_of_circle.py
@@ -0,0 +1,4 @@
+pi = 3.142
+rad = float(input("Enter the value of radius of the circle\n"))
+area = pi * rad * rad
+print('The area of circle with radius',rad,'is',area)
