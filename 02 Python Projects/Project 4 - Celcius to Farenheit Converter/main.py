49835fce755a787ea1ff81da4267b7cf4bd65694
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-07-13 10:19:09 +0530

    commit

---
+++ 02 Python Projects/Project 4 - Celcius to Farenheit Converter/main.py
@@ -0,0 +1,3 @@
+celcius = float(input("Enter Temperature in Celsius: "))
+farenheit = (celcius * (9/5)) + 32
+print(f"{int(celcius)} Celcius = {int(farenheit)} Farenheit")
---
+++ 02 Python Projects/Project 5 - Trip Cost Calculator/main.py
@@ -0,0 +1,8 @@
+print("Welcome to the Trip Cost Calculator!")
+days = input("How many days will you stay? ")
+costPerNight = input("How much does hotel cost per night? $")
+flightCost = input("How much does flight cost? $")
+rentalCar = input("If you need rental car please enter the price otherwise enter zero. $")
+others = input("Enter other possible expenses $")
+total = float(days)*float(costPerNight)  + float(flightCost) + float(rentalCar)*float(days)  + float(others)
+print(f"Total Cost: ${round(total,2)}")
