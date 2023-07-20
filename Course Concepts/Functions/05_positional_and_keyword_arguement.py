29c7078366b292cc66dfc20cd3d729dd9b6ff10e
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-07-20 09:04:22 +0530

    commit

--- Course Concepts/Functions/05_positional_and_keyword_arguement.py
+++ Course Concepts/Functions/05_positional_and_keyword_arguement.py
@@ -1,11 +1,9 @@
-#function with passing of name and city
+# function with passing of name and city
 
 
+# positional arguements
 
-
-#positional arguements
-
-'''
+"""
         While passing the different arguements to a function during its call, 
     the positions of the passed values/arguements matters a lot.
         The first arguement will be passed to the first parameter in the function definition , 
@@ -16,31 +14,32 @@
     The programmer may do mistake while calling the function and passing the arguements every time.
     To overcome the issue and confirm that the arguement should reach the intended parameter, keyword arguements are used.
 
-''' 
+"""
 
-def greet_with_name(name,city):
-    print(f"Hello {name}!") 
+
+def greet_with_name(name, city):
+    print(f"Hello {name}!")
     print(f"How is the weather like in {city}?")
+
 
 naam = input("Enter your name please: ")
 sheher = input("Enter your city: ")
 greet_with_name(naam, sheher)
 
 
-
-
-
-#keyword arguements
-'''
+# keyword arguements
+"""
 The keyword arguements are the arguements with keywords assigned to them 
 during the time of the function call itself so that theres no chance of mismatch 
 even when the arguements are passed in mismatched positions.
 
-'''
+"""
 
-def greet_with_name(a,b):
-    print(f"Hello {a}!") 
+
+def greet_with_name(a, b):
+    print(f"Hello {a}!")
     print(f"How is the weather like in {b}?")
+
 
 naam = input("Enter your name please: ")
 sheher = input("Enter your city: ")
