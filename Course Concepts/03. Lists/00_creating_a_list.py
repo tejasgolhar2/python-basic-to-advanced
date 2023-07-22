69f23efb37f26965948b7a4275899777f9834fe0
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-07-22 22:17:39 +0530

    commit

---
+++ Course Concepts/03. Lists/00_creating_a_list.py
@@ -0,0 +1,7 @@
+# Lists in Python are defined using square brackets [] and can hold elements 
+# of different data types, including numbers, strings, other lists, or even 
+# complex objects
+
+my_list = [1, 2, 3, 4, 5]
+fruits = ["apple", "banana", "orange"]
+mixed_list = [1, "hello", 3.14, True]
