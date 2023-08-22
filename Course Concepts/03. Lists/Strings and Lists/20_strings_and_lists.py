967e34cfc500458fe4ca7376ed05971711a36057
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-08-22 10:14:27 +0530

    commit

---
+++ Course Concepts/03. Lists/Strings and Lists/20_strings_and_lists.py
@@ -0,0 +1,27 @@
+#   Conversion of strings into lists
+
+# here we use list function that converts the characters in the string as the individual elements of the string
+# the list elements then can be accessed by indexing
+
+# case 1
+# each character as the element
+custom_string = "tejas"
+custom_list = list(custom_string)
+print(custom_list)
+
+# case 2
+# each word as the element [ USE OF SPLIT METHOD ]
+# the split method is used to convert the string into list with set of characters separated by space separator
+# no need to specify the typeconverter before the split operator
+
+# Ex1
+string1 = "You are a good boy"
+list1 = string1.split(" ")
+print(list1)
+
+# Ex2
+string2 = "You_are_a_bad_boy"
+list2 = string2.split("_")
+print(list2)
+
+ 
