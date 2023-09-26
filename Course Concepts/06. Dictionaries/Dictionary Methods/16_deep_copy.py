f06069b8a4c30f5e2c06c52dd3d2ed02c23f4dd6
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-09-26 19:24:09 +0530

    commit

---
+++ Course Concepts/06. Dictionaries/Dictionary Methods/16_deep_copy.py
@@ -0,0 +1,31 @@
+# CONCEPT OF DEEP COPY
+#       This results in changes in the copied dictionary only and doesn't affect the original one
+#    It uses the inclusion of the "copy" library
+
+import copy
+
+
+name_list = ["Edy", "John", "Ewan"]
+city_list = ["London", "Berlin", "Paris"]
+language_list = ["Enlish", "German", "French"]
+
+person = {
+    "name" : name_list,
+    "city" : city_list,
+    "language" : language_list,
+}
+
+new_person = copy.deepcopy(person)
+
+person["city"].append("Appended using person dict")
+
+new_person["city"].append("Appended using new person dict")
+
+city_list.append("Append by itself") 
+
+
+# Only the changes as per the calling of dictionary is done 
+
+print(person["city"])
+print(new_person["city"])
+print(city_list)
