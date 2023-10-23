877d59f8e1a5ed9ca809630f7923cbb8d7b12285
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-10-23 19:44:34 +0530

    commit

---
+++ Coding Exercises/57_fing_most_frequent_item.py
@@ -0,0 +1,21 @@
+# Find Most Frequent Item
+
+#   Implement a function which takes a tuple as a parameter and returns another 
+# tuple with two elements. First element is the most frequent item and the second 
+# element of number of repetition.
+
+# Hint: Use count() method
+
+# Example
+
+# my_tuple = ("a","b","c","d","e","a","c","e","b","e","c","a","f","e","r")
+# most_frequent(my_tuple)
+# Output
+
+# ('e', 4)
+
+def most_frequent(v_tuple):
+    j
+
+my_tuple = ("a","b","c","d","e","a","c","e","b","e","c","a","f","e","r")
+print(most_frequent(my_tuple))
