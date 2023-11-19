f5d0a32ca85c4acedd5b06ecc4d0c769e132fe8d
tejasgolhar2 <tejasgolhar2@gmail.com>
2023-11-19 19:45:30 +0530

    commit

--- Advanced Programming - 7th Sem/3. Question Bank Problems/Unit 2/08_duplicates_using_dictionary.py
+++ Advanced Programming - 7th Sem/3. Question Bank Problems/Unit 2/08_duplicates_using_dictionary.py
@@ -1,6 +1,18 @@
 # Write a program to find all duplicate characters in strings using dictionary.
 
 def duplicate(v_string):
+    n_dict = dict()
+    for char in v_string:
+        if char not in n_dict:
+            n_dict[char] = 1
+        else:
+            val = n_dict[char]
+            n_dict[char] = val + 1
+
+    for key,value in n_dict.items():
+        if value >= 2:
+            print(key)
 
 
 value = input("Enter the string value for checking duplicates: ")
+duplicate(value)
